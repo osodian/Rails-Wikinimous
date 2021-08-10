0384f76c49d5484be337a3d7afe3c4f55dd0dafb
Oscar Sodian <oscar.sodian@gmx.de>
2021-08-10 14:12:12 +0200

    Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+
+  get 'article/:id/edit', to: 'articles#edit', as: :edit
+  get 'article/:id', to: 'articles#show', as: :article
+
+  delete 'article/:id', to: 'articles#destroy'
+  patch 'article/:id', to: 'articles#update'
+  post 'articles', to: 'articles#create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
